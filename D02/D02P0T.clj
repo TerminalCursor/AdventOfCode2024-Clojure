844a2e35f0df0e581fc5488e2e70df78c759ebaf
TerminalCursor <TerminalCursor@localhost>
2024-12-01 19:52:38 -0800

    Add Automatic Submission

--- D02/D02P0T.clj
+++ D02/D02P0T.clj
@@ -4,3 +4,9 @@
 
 (def lines (->> (slurp "test-input.txt")
                 str/split-lines))
+
+; (load-file "../TEST/post.clj")
+;
+; (def response (aoc-post/post-answer 2024 1 1 11))
+; (println "Response Body" (:body response))
+; (println "Status Code" (:status response))
