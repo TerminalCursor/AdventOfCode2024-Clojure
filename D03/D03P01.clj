481436183e35d959dd5e5e38cfb8cc4587b06582
TerminalCursor <TerminalCursor@localhost>
2024-12-03 09:23:59 -0800

    Cleanup

--- D03/D03P01.clj
+++ D03/D03P01.clj
@@ -2,20 +2,25 @@
   (:require [clojure.string :as str]
             [clojure.core :as core]))
 
-(defn split-empty [s]
-  (vec (map Integer/parseInt (str/split s #" +"))))
-
 (def lines (->> (slurp "input-d03p01.txt")))
 
 (def product 0)
-(doseq [% (vec (core/re-seq #"mul\([0-9]{1,3},[0-9]{1,3}\)" lines))]
-  (def numerals (core/re-seq #"[0-9]{1,3}" %))
-  (def n1 (Integer/parseInt (nth numerals 0)))
-  (def n2 (Integer/parseInt (nth numerals 1)))
-  (def product (+ product (* n1 n2))))
+
+(def match (vec (core/re-seq #"mul\(([0-9]{1,3}),([0-9]{1,3})\)|do\(\)|don't\(\)" lines)))
+
+
+(doseq [% match]
+  (def op (nth % 0))
+  (def n1 (nth % 1))
+  (def n2 (nth % 2))
+  (when-not (= n1 nil)
+    (def n2 (Integer/parseInt n2))
+    (def n1 (Integer/parseInt n1))
+    (def product
+      (+ product (* n1 n2)))))
 (println product)
 
-;(def answer product)
+;(def answer)
 ;(load-file "../TEST/post.clj")
 ;(def response (aoc-post/post-answer 2024 3 1 answer))
 ;(println "Response Body" (:body response))
--- D03/D03P02.clj
+++ D03/D03P02.clj
@@ -2,18 +2,11 @@
   (:require [clojure.string :as str]
             [clojure.core :as core]))
 
-(defn split-empty [s]
-  (vec (map Integer/parseInt (str/split s #" +"))))
-
-;(def lines (->> (slurp "test-input.txt")))
 (def lines (->> (slurp "input-d03p01.txt")))
 
 (def product 0)
 
 (def match (vec (core/re-seq #"mul\(([0-9]{1,3}),([0-9]{1,3})\)|do\(\)|don't\(\)" lines)))
-(println match)
-
-(doseq [% match])
 
 (def doing true)
 (doseq [% match]
@@ -26,10 +19,10 @@
         (def doing false))
       (if (= true doing)
         (do
-    (def n2 (Integer/parseInt n2))
-    (def n1 (Integer/parseInt n1))
-    (def product
-      (+ product (* n1 n2)))))))
+          (def n2 (Integer/parseInt n2))
+          (def n1 (Integer/parseInt n1))
+          (def product
+            (+ product (* n1 n2)))))))
 (println product)
 
 ;(def answer product)
