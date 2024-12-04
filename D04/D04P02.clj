f774a87e029fc55a2bdb544cd4592a866a57095f
TerminalCursor <TerminalCursor@localhost>
2024-12-03 22:15:29 -0800

    Day 4
    TODO: Cleanup

--- D04/D04P02.clj
+++ D04/D04P02.clj
@@ -2,18 +2,50 @@
   (:require [clojure.string :as str]
             [clojure.core :as core]))
 
-(defn split-empty [s]
-  (vec (map Integer/parseInt (str/split s #" +"))))
-
-(def lines (->> (slurp "input-d02p01.txt")
+(def lines (->> (slurp "input-d04p01.txt")
+;(def lines (->> (slurp "test-input.txt")
                 str/split-lines))
 
-(def numbers (map split-empty lines))
+(println (str/join "\n" lines))
 
-(defn parse-row [row])
+(defn get [mat x y]
+  (nth (nth mat y) x))
+
+(def y-c (count lines))
+(def x-c (count (first lines)))
+(def exes (for [x (range 1 (- x-c 1))]
+           (for [y (range 1 (- y-c 1))]
+             (list
+              (str
+               (get lines (- x 1) (- y 1))
+               (get lines x y)
+               (get lines (+ x 1) (+ y 1)))
+              (str
+               (get lines (- x 1) (+ y 1))
+               (get lines x y)
+               (get lines (+ x 1) (- y 1)))
+              x
+              y
+              ))))
+
+(def truths 0)
+(doseq [exe exes]
+  (doseq [ex exe]
+    (def a (nth ex 0))
+    (def b (nth ex 1))
+    (if (and
+         (or (= a "MAS")
+             (= a "SAM"))
+         (or (= b "MAS")
+             (= b "SAM")))
+      (do
+        (def truths (+ truths 1))
+        ))))
+
+(println truths)
 
 ;(def answer)
 ;(load-file "../TEST/post.clj")
-;(def response (aoc-post/post-answer 2024 4 2 answer))
+;(def response (aoc-post/post-answer 2024 4 1 answer))
 ;(println "Response Body" (:body response))
 ;(println "Status Code" (:status response))
--- D04/D04P0T.clj
+++ D04/D04P0T.clj
@@ -2,15 +2,35 @@
   (:require [clojure.string :as str]
             [clojure.core :as core]))
 
-(defn split-empty [s]
-  (vec (map Integer/parseInt (str/split s #" +"))))
-
 (def lines (->> (slurp "test-input.txt")
                 str/split-lines))
 
-(def numbers (map split-empty lines))
+(println (str/join "\n" lines))
+(def diagonals-a lines)
+(def diagonals-b (for [n (range (count diagonals-a))]
+                   (str (str/join (take n (repeat "."))) (nth diagonals-a n))))
+(def diagonals-1 (apply mapv str (for [n (range (count diagonals-a))]
+                                   (str (str/join (take n (repeat "."))) (nth diagonals-a n) (str/join (take (- (count diagonals-a) n) (repeat ".")))))))
+(def diagonals-2 (apply mapv str (for [n (range (count diagonals-a))]
+                                   (str (str/join (take n (repeat "."))) (str/reverse (nth diagonals-a n)) (str/join (take (- (count diagonals-a) n) (repeat ".")))))))
 
-(defn parse-row [row])
+(def lines (concat '("Original") lines
+                   '("Reversed") (map str/reverse lines)
+                   '("T") (apply map str lines)
+                   '("Reversed T") (apply map str (reverse lines))
+                   '("DownLeft D") diagonals-1
+                   '("DownRight D") diagonals-2
+                   '("UpRight D") (map str/reverse diagonals-1)
+                   '("DownRight D") (map str/reverse diagonals-2)))
+(println lines)
+
+(def counter 0)
+(doseq [line lines]
+  (def matches (core/re-seq #"XMAS" line))
+  (println (count matches) line matches)
+  (def counter (+ counter (count matches))))
+(println counter)
+;(println diagonals)
 
 ;(def answer)
 ;(load-file "../TEST/post.clj")
